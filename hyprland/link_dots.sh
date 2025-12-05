6a21df2ae904278b5e7376d951cbd906a6fab0b3
bae <hortock@outlook.com>
2025-12-05 19:05:51 -0300

    Minor ajusts in link script

--- hyprland/link_dots.sh
+++ hyprland/link_dots.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 SCRIPT_ROOT="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
-ln -f $SCRIPT_ROOT/config/* -t $HOME/.config/
-ln -f $SCRIPT_ROOT/.bashrc $HOME/.bashrc
-ln -f $SCRIPT_ROOT/.bashrc $HOME/.vimrc
+ln -sf $SCRIPT_ROOT/config/* -t $HOME/.config/
+ln -sf $SCRIPT_ROOT/.bashrc $HOME/.bashrc
+ln -sf $SCRIPT_ROOT/.bashrc $HOME/.vimrc
